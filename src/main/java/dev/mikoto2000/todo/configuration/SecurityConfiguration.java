f845c3951b85141e67bde0907e18edf6d9eb18e3
mikoto2000 <mikoto2000@gmail.com>
2025-03-30 11:59:51 +0000

    ログアウト機能の追加。

--- src/main/java/dev/mikoto2000/todo/configuration/SecurityConfiguration.java
+++ src/main/java/dev/mikoto2000/todo/configuration/SecurityConfiguration.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     /**
      * 認可のための SecurityFilterChain を設定
@@ -25,11 +33,25 @@ public class SecurityConfiguration {
         .logout(logout -> logout
             .logoutUrl("/logout")
             .logoutSuccessUrl("/")
+            .invalidateHttpSession(true)
+            .clearAuthentication(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(oidcLogoutSuccessHandler())
+            .permitAll()
         )
         .oauth2Login();
 
         http.csrf();
 
         return http.build();
+    }
+
+    LogoutSuccessHandler oidcLogoutSuccessHandler() {
+      OidcClientInitiatedLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler =
+              new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+      oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
+
+      return oidcClientInitiatedLogoutSuccessHandler;
     }
 }
